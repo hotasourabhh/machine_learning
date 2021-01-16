90ae43fdd10d56c9a01684ed681fcb0c8698dbb5
unknown <hota.saurabh@gmail.com>
2021-01-16 11:44:46 +0530

    Gradient descent chekin

--- ml practice/linear_regression/gradient_descent.m
+++ ml practice/linear_regression/gradient_descent.m
@@ -1,4 +1,17 @@
-function [jVal] = gradient_descent(alpha,theta,x)
-  jVal = 
+function [jHistory,theta] = gradient_descent(X,Y,alpha,theta,iterations)
+  m = rows(X); %number of samples
+  jHistory = zeros(iterations,1);
   
+  %running loop for number of iterations to 
+  %find theta for which cost function is minimum
+  for i = 1:iterations
+    hTheta = X * theta; %hypothesis funtion
+    temp0 = theta(1) - alpha*(1/m)*sum((hTheta - Y));
+    temp1 = theta(2) - alpha*(1/m)*sum((hTheta - Y).*X(:,2));
+    theta(1) = temp0;
+    theta(2) = temp1;
+    
+    jHistory(i) = costFunc(X,Y,theta);
+  end  
+   
 endfunction
